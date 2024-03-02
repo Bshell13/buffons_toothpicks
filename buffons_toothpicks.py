ff6d86586ddb29607da5cc0fad8af1ad10122a10
Brandon Shellenberger <brandonshellenberger93@gmail.com>
2024-03-02 08:58:57 -0600

    commit and push setup

---
+++ buffons_toothpicks.py
@@ -0,0 +1,14 @@
+"""
+Buffon's "Toothpick" Experiment
+
+Brandon Shellenberger
+
+This project is for my students to discover Buffon's Needle Experiment.
+I will gather all the data from what my students have made.
+"""
+
+# Standard Libraries
+import numpy as np
+import pandas as pd
+import pyarrow
+
